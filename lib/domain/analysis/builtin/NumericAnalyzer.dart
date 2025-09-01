189ad3c3df07f03d212a2c68c7119c7cf4295a34
youngdevel <youngdevel@hotmail.com>
2025-09-01 23:36:16 +0800

    [analysis] feat: implement NumericAnalyzer
    Replace placeholder code with actual implementation for NumericAnalyzer

--- lib/domain/analysis/builtin/NumericAnalyzer.dart
+++ lib/domain/analysis/builtin/NumericAnalyzer.dart
@@ -3,6 +3,8 @@
 // GPLv3 license. Use of this file is governed by terms and conditions that
 // can be found in the COPYRIGHT file.
 
+import 'dart:math' show pow, sqrt;
+
 import '../Analyzer.dart';
 import 'shared.dart';
 
@@ -33,23 +35,76 @@ final class NumericAnalyzer extends VectorAnalyzer {
   @override
   Map<String, dynamic> analyze(
     Iterable<GenericCandidate> vector, Map<String, Object> options
-  ) => {
-    "type": "column",
-    "args": {
-      "children": [
-        {
-          "type": "text",
-          "args": {
-            "text": "Average",
-            "style": {
-              "color": "#000",
-              "fontSize": 18
-            }
+  ) {
+    if(options case {
+      "summation":          final bool summationOpt,
+      "average":            final bool averageOpt,
+      "median":             final bool medianOpt,
+      "maximum":            final bool maximumOpt,
+      "minimum":            final bool minimumOpt,
+      "extremeDeviation":   final bool extremeDeviationOpt,
+      "standardDeviation":  final bool standardDeviationOpt,
+      "interquartileRange": bool interquartileRangeOpt
+    }) {
+      final data = vector.first.data.cast<num>();
+      final length = data.length;
+      interquartileRangeOpt = interquartileRangeOpt && (length > 1);
+
+      num summation = 0, average = 0, variance = 0;
+      if(summationOpt || averageOpt || standardDeviationOpt) {
+        summation = data.reduce((lhs, rhs) => lhs + rhs);
+        if(averageOpt) average = summation / length;
+        if(standardDeviationOpt) variance = data.map(
+          (val) => pow(val - average, 2)
+        ).reduce((lhs, rhs) => lhs + rhs) / length;
+      }
+      num median = 0, maximum = 0, minimum = 0, interquartileRange = 0;
+      if(medianOpt || interquartileRangeOpt) {
+        final sorted = data.toList()..sort();
+        minimum = sorted.first;
+        maximum = sorted.last;
+        median = _calcMedian(sorted);
+        if(interquartileRangeOpt) {
+          List<num> lowerHalf, upperHalf;
+          if (length % 2 == 1) {
+            // If n is odd, exclude the median from both halves.
+            final medianIndex = length ~/ 2;
+            lowerHalf = sorted.sublist(0, medianIndex);
+            upperHalf = sorted.sublist(medianIndex + 1);
+          } else {
+            // If n is even, split the array evenly.
+            final midIndex = length ~/ 2;
+            lowerHalf = sorted.sublist(0, midIndex);
+            upperHalf = sorted.sublist(midIndex);
           }
+
+          final q1 = _calcMedian(lowerHalf);
+          final q3 = _calcMedian(upperHalf);
+
+          interquartileRange = q3 - q1;
         }
-      ]
-    }
-  };
+      } else if(
+        maximumOpt || minimumOpt || extremeDeviationOpt
+      ) for(final val in data) {
+        if(val > maximum) maximum = val;
+        if(val < minimum) minimum = val;
+      }
+      return {
+        "type": "single_child_scroll_view",
+        "args": { "child": Padded.symmetric(30, 15, MultiColumn([ [
+          LabeledAttribute("Average: ", averageOpt ? average : "-"),
+          LabeledAttribute("Median: ", medianOpt ? median : "-"),
+          LabeledAttribute("Maximum: ", maximumOpt ? maximum : "-"),
+          LabeledAttribute("Minimum: ", minimumOpt ? minimum : "-"),
+        ], [
+          LabeledAttribute("Extreme Deviation: ", extremeDeviationOpt ? maximum - minimum : "-"),
+          LabeledAttribute("Standard Deviation: ", standardDeviationOpt ? sqrt(variance) : "-"),
+          LabeledAttribute("Interquartile Range: ", interquartileRangeOpt ? interquartileRange : "-"),
+          LabeledAttribute("Summation: ", summationOpt ? summation : "-"),
+        ] ], 36, 12)) }
+      };
+    } else return {}; // unreachable
+  }
 
   @override
   String get name => "Numeric";
@@ -65,4 +120,14 @@ final class NumericAnalyzer extends VectorAnalyzer {
     "standardDeviation": true,
     "interquartileRange": true
   };
+
+  num _calcMedian(List<num> sorted) {
+    if(sorted.length % 2 == 1) {
+      return sorted[sorted.length ~/ 2];
+    } else {
+      final mid1 = sorted[sorted.length ~/ 2 - 1];
+      final mid2 = sorted[sorted.length ~/ 2];
+      return (mid1 + mid2) / 2;
+    }
+  }
 }
